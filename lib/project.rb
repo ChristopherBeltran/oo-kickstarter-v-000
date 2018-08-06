4019341e6d68d9d052c15828fca1a266b4f323b5
Christopher Beltran <christod.beltran@gmail.com>
2018-08-06 00:50:29 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,11 @@
 class Project
-  attr_accessor :title, :backer
+  attr_accessor :title, :backers
   
 def initialize(title)
   @title = title
+  @backers = []
 end 
+
+def add_backer(backer)
+  @backers << backer
 end 
